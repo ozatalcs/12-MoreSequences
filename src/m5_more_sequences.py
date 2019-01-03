e93bafb1102710fa911f886cda6e11024e1dc36f
ozatalcs <ozatalcs@rose-hulman.edu>
2019-01-03 13:40:48 -0500

    Conner Ozatalar Day 12

--- src/m5_more_sequences.py
+++ src/m5_more_sequences.py
@@ -155,7 +155,7 @@ def count_last_n_odds(integers, n):
       :rtype: int
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # Done: 3. Implement and test this function.
     #     The testing code is already written for you (above).
     # -------------------------------------------------------------------------
     count = 0
@@ -234,10 +234,13 @@ def index_of_first_negative(numbers):
       :rtype: int
     """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # Done: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     # -------------------------------------------------------------------------
-
+    for k in range(len(numbers)):
+        if numbers[k] < 0:
+            return k
+    return -1
 
 def run_test_contains_an_a():
     """ Tests the   contains_an_a   function. """
@@ -292,7 +295,7 @@ def contains_an_a(s):
       :rtype: bool
     """
     # -------------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # Done: 5. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     ###########################################################################
@@ -305,7 +308,10 @@ def contains_an_a(s):
     #   Use an explicit loop, as you have done in the other problems.
     #   No fair using the   count   or   find   string methods.
     # -------------------------------------------------------------------------
-
+    for k in range(len(s)):
+        if s[k] is 'a':
+            return True
+    return False
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
